1e110159dce86c07e09185537828b33ec2dac123
Mihail <mishgun032@yandex.com>
2022-06-07 18:43:31 +0100

    reused the popup to show the user that he finished the game

--- source.js
+++ source.js
@@ -13,11 +13,12 @@ async function getJson(url){
   }
 }
 const level = await getJson("./level.json")
-let currentLevel = 2
+let currentLevel = 1
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
+const popup = document.getElementById("popup")
 
 /*
 window.level will be used as global variable across all the modules to get the current level 
@@ -38,7 +39,7 @@ const physicsEngine = new PhysicsEngine(world)
 const renderEngine = new RenderEngine(30,world.update,screen.update,physicsEngine)
 
 const retryBtn = document.getElementById("retry") 
-retryBtn.addEventListener("click", click => { restartLevel();document.getElementById("popup").style.display="none"; })
+retryBtn.addEventListener("click", click => { popup.style.display="none";restartLevel(); })
 window.addEventListener("keydown", cfg.handlOnKeyDown)
 window.addEventListener('keyup',cfg.handlOnKeyUp)
 window.addEventListener('resize', () => screen.onResize(document.documentElement.clientWidth, document.documentElement.clientHeight, world.height / world.width))
@@ -57,11 +58,14 @@ function startLevel(){
   renderEngine.start()
 }
 
-function retry(){
+function retry(finish=false){
   setTimeout( () => renderEngine.stop(), 0) //yes
-  document.getElementById("popup").style.display = "flex";
+  popup.style.display = "flex";
+  document.getElementById("title").innerHTML = finish ? "Finish" : "Game Over"
   document.getElementById("score").innerHTML = `Score: ${world.score}`
-//  document.getElementById("time").innerHTML += world.timer
+  document.getElementById("time").innerHTML = `Time Left: ${world.timer}s`
+  currentLevel = 1
+  window.level = level[currentLevel]
 }
 function restartLevel(){
   renderEngine.stop()
@@ -78,9 +82,23 @@ function restartLevel(){
 function nextLevel(){
   renderEngine.stop()
   currentLevel += 1
+
+  setTimeout( () => renderEngine.stop(),0)
+  if (currentLevel == 3){
+    clearInterval(timer)
+    retry(true)
+    return;
+  }else { 
+    currentLevel += 1
+  }
+  world.clearWorld()
+  physicsEngine.clearCollisionTable()
   window.level = level[currentLevel]
-  world.player.health = 1 //
-  world.collisionTable = {}
+  world.timer = window.level.timer
+//  world.player.health = 1 //
+
+  //start lvl will set a new interval so we need to clear the old one to prevent mu
+  clearInterval(timer)
   startLevel()
 }
 startLevel()
