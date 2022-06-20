e59f0e53af54fddf32182316e0ee26339305f2f8
Mihail <mishgun032@yandex.com>
2022-06-20 19:46:57 +0100

    refactor the code changed the way the world is updated and reset

--- world.js
+++ world.js
@@ -14,12 +14,17 @@ export default class World{
     this.delete = this.delete.bind(this)
     this.player = new Player(0,0)//the passed params are the spawn points coordinates
     this.objects = [ //objects are all the enemies and the obstacles in the world e.g fireball 
+		     /*
+			all the objects here are pseudo arrays
+			which will store different objects like enemies,projectiles etc
+			starting at index 0 
+		      */
 		     {},//coins
 		     {},//mushrooms
 		     {},//plants
 		     {}//projectiles
     ]
-    
+    this.objectsPossition = {}
     this.populateWorld = this.populateWorld.bind(this)
     this.update = this.update.bind(this)//update gets called from the render which makes the context (this) get lost 
     this.increaseScore = this.increaseScore.bind(this)
@@ -39,28 +44,32 @@ export default class World{
 
   }
   clearWorld(){
+    this.timer=window.level.timer
+    this.score = 0
+    this.player.health = 100
     this.objects = [{},{},{},{}]
+    this.objectsPossition = {}
   }
   populateWorld(){
     this.player.x = window.level.mcSpawn[0]*16//set the spawn spoints here
     this.player.y = window.level.mcSpawn[1]*16
 
-    window.level.coins.forEach( ([x,y],index) => {
+    window.level.coins ? window.level.coins.forEach( ([x,y],index) => {
       this.objects[0][index] = new Coin(x*16,y*16, () => this.delete(0,index), () => this.increaseScore())
-    });
-    window.level.mushrooms.forEach( (mushroom,index) => {
+    }) : null
+    window.level.mushrooms ? window.level.mushrooms.forEach( (mushroom,index) => {
       this.objects[1][index] = new Mushroom(mushroom.x*16,mushroom.y*16,mushroom.bariers, () => this.delete(1,index) )
-    })
-    window.level.plants.forEach( (plant,index) => {
+    }) : null 
+    window.level.plants ? window.level.plants.forEach( (plant,index) => { 
       this.objects[2][index] = new Plant(plant.x*16,plant.y*16, plant.vector, () => this.delete(2,index) )
-    })
+    }) : null
   }
+
   update(){
     if(this.timer == 0){
       this.retry()
     }
     if(this.player.health <= 0){
-      console.log(this.player.health)
       this.retry()
     }else if(this.player.x == window.level.endGame[0]*16){
       if(this.player.y == window.level.endGame[1]*16){
@@ -68,10 +77,27 @@ export default class World{
       }
     }
     this.player.update()
-    this.objects.slice(1).forEach( object => {
+    this.objects.slice(1).forEach( (object,index) => {//start from 1 objs as object at index 0 stores all the coins which do not need to be updated
       for(let key in object){//only the enemies have an inner state to update 
 	object[key].update()
+//	this.objectsPossition[object[key].x] = new objectPosition(object[key].y,[index,key]) //development 
       }
     })
   } 
 }
+
+class objectPosition{
+  constructor(y,index){
+    this.y = {
+      index: index
+    }
+  }
+}
+
+let x = {
+  y: {
+    f:"5"
+  }
+}
+
+console.log(x.y.f)
